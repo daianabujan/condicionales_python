59617a7476d669db365b2c970d228e022b07609f
daianabujan <daiana.bujan@gmail.com>
2022-03-23 19:44:40 -0300

    Ejercicios Realizados

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -18,19 +18,37 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 
 # Compare cual de las dos palabras es mayor (alfabéticamente)
 # Imprima en pantalla según corresponda
-
+if texto_1 > texto_2:
+    print('{} es mayor que {}'.format(texto_1, texto_2))
+elif texto_1 == texto_2:
+    print('{} es igual que {}'.format(texto_1, texto_2))
+else:
+    print('{} es mayor que {}'.format(texto_2, texto_1))
+ 
 # Compare cual de las dos palabras tiene mayor
 # cantidad de letras
 # Imprima en pantalla según corresponda
-
+if len(texto_1) > len(texto_2):
+    print('{} posee mayor cantidad de letras que {}'.format(texto_1, texto_2))
+elif len(texto_1) == len(texto_2):
+    print('{} posee la misma cantidad de letras que {}'.format(texto_1, texto_2))
+else:
+    print('{} posee mayor cantidad de letras que {}'.format(texto_2, texto_1))
+ 
 # Verifique si la primera letra de la primera palabra
 # es mayor a la primera letra de la segunda palabra
 # Imprima en pantalla según corresponda
-
+ 
 copia_texto_1 = texto_1  # Copia de la variable texto_1
-
+ 
 # Verifique que copia_texto_1 es igual a texto_1
 # Imprima en pantalla según corresponda
-
+if copia_texto_1 == texto_1:
+    print('copia_texto_1({}) es igual a texto_1'.format(copia_texto_1))
+ 
 # Verifique que copia_texto_1 es distinta a texto_2
 # Imprima en pantalla según corresponda
+if (copia_texto_1 == texto_2):
+    print('copia_texto_1({}) es igual a texto_2'.format(copia_texto_1))
+else:
+    print('copia_texto_1({}) es diferente a texto_2'.format(copia_texto_1))
